292f6d421a27be2f88086e6d16556644492d0969
Andrew Sudduth <andrewmsudduth@gmail.com>
2022-04-29 12:53:39 -0500

    Setup DI Provider, Wrap Console, and setup failing test for Hello World

--- PhotoAlbum/Program.cs
+++ PhotoAlbum/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using PhotoAlbum.Services;
+using PhotoAlbum.Wrappers;
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IConsoleWrapper, ConsoleWrapper>()
+    .BuildServiceProvider();
+
+var consoleService = serviceProvider.GetRequiredService<IConsoleService>();
+
+consoleService.StartApplication();
